6b2651b997ab376a4045f9bf741f9306c027c02e
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2023-02-26 18:58:13 +0000

    fix: lint erros

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,7 @@ import style from './Footer.module.css'
 export default function FooterComponent() {
   return (
     <footer className={style.footer}>
-      <small>Desenvolvido por <a href="https://github.com/luizdeveloperbr" target="_blank" >@luizdeveloperbr</a> </small>
+      <small>Desenvolvido por <a href="https://github.com/luizdeveloperbr" rel='noreferrer' target="_blank" >@luizdeveloperbr</a> </small>
     </footer>
   );
 }
--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import style from './Header.module.css'
 export default function HeaderComponent() {
     return (
         <header>
             <div className={style.avatar_conteiner}>
-                <img 
+                <Image 
+                alt="Profile Pic"
                 className={style.avatar_image}
                 src="https://via.placeholder.com/48/?text=LE"
                 height={48}
