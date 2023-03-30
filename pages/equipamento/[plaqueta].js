a7fe5add7e8ea1b5e809d25d2379a85af59a020c
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2023-03-30 01:20:18 -0300

    Update [plaqueta].js

--- pages/equipamento/[plaqueta].js
+++ pages/equipamento/[plaqueta].js
@@ -1,4 +1,7 @@
 import { axiosInstance } from "../../lib/axios";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
 export async function getServerSideProps({params: {plaqueta}}){
   const {data} = await axiosInstance.request({url: `/equipamentos/${plaqueta}`, method: 'GET'})
   return{
@@ -8,9 +11,55 @@ export async function getServerSideProps({params: {plaqueta}}){
   }
 }
 export default function EquipamentoPagePorId({equipamento}){
+  const router = useRouter()
+    return (
+    <section className="drawer-content">
+      <header>
+          <div className="text-sm breadcrumbs bg-base-200 rounded-md pl-2 mb-4">
+            <ul>
+              <li>
+                <a onClick={() => router.push({pathname: '/'})}>Home</a>
+              </li>
+              <li>
+                <a onClick={() => router.back()}>Equipamentos</a>
+              </li>
+              <li>{equipamento.plaqueta}</li>
+            </ul>
+          </div>
+      </header>
+      <div className="flex flex-row justify-between">
+        <div className="bg-base-200 rounded-md w-full">
+          <figure className="text-center">
+            <Image className="mx-auto my-10" src="https://api.lorem.space/image/furniture?w=250&h=250" alt="foto do bem patrimonial" width={250} height={250} />
+            <figcaption>Nº Serie</figcaption>
+          </figure>
+        </div>
+        <div className="divider-horizontal divider"></div>
+        <div className="bg-base-200 rounded-md w-full p-3">
+          <form>
+            <fieldset className=" border-zinc-300 border-2 rounded p-2">
+              <legend>Equipamento</legend>
+            <input className="input mb-1 input-bordered input-sm w-full" value={equipamento.descricao} readOnly type="text" />
+            <input className="input mb-1 input-bordered input-sm" value={equipamento.plaqueta} readOnly type="text" />
 
-  return (<>
-    <h2>{equipamento.descricao}</h2>
-  </>
+            </fieldset>
+            <div className="btn-group gap-1">
+                  <button className="btn btn-sm btn-primary">
+                    Emprestimo
+                  </button>
+                  <button className="btn btn-sm btn-primary">
+                    Manutenção
+                  </button>
+                  <button className="btn btn-sm btn-primary">
+                    Transferência
+                  </button>
+                  <button className="btn btn-sm btn-primary">
+                    Baixa
+                  </button>
+                </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
