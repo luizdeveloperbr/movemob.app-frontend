cda24d4d5957f93b34e0cf95d94ee78bbbaeae9a
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2023-03-30 01:14:59 +0000

    BreadCrumps component

--- pages/equipamento/[plaqueta].js
+++ pages/equipamento/[plaqueta].js
@@ -1,6 +1,4 @@
 import { axiosInstance } from "../../lib/axios";
-import { useRouter } from "next/router";
-
 export async function getServerSideProps({params: {plaqueta}}){
   const {data} = await axiosInstance.request({url: `/equipamentos/${plaqueta}`, method: 'GET'})
   return{
@@ -10,21 +8,9 @@ export async function getServerSideProps({params: {plaqueta}}){
   }
 }
 export default function EquipamentoPagePorId({equipamento}){
-  const router = useRouter()
-    return (
-    <section className="drawer-content">
-          <div className="text-sm breadcrumbs">
-            <ul>
-              <li>
-                <a onClick={() => router.push({pathname: '/'})}>Home</a>
-              </li>
-              <li>
-                <a onClick={() => router.back()}>Equipamentos</a>
-              </li>
-              <li>{equipamento.plaqueta}</li>
-            </ul>
-          </div>
-          <h2>{equipamento.descricao}</h2>
-    </section>
+
+  return (<>
+    <h2>{equipamento.descricao}</h2>
+  </>
   );
 }
